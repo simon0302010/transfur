bb764dc896fd41b639282b65717e95b06ea3dbfd
simon0302010 <simon0302010@gmail.com>
2026-09-22 18:35:24 +0200

    feat: not finished with this

--- src/gui/gui_linux.c
+++ src/gui/gui_linux.c
@@ -3,10 +3,20 @@ Source file for Linux GUI code.
 Needs to implement all functions defined in `gui.h`
 */
 
+#include <stdlib.h>
 #define NK_IMPLEMENTATION
+#define NK_INCLUDE_DEFAULT_ALLOCATOR
+#define NK_INCLUDE_FONT_BAKING
+#define NK_INCLUDE_DEFAULT_FONT
 #include "nuklear/nuklear.h"
+
+struct nk_context ctx;
 
 int create_window(const char *title)
 {
-        return 0;
+        nk_init(&ctx, NULL, NULL);
+        if (nk_begin(&ctx, "Transfur", nk_rect(10, 10, 400, 300), NK_WINDOW_BORDER | NK_WINDOW_MOVABLE))
+        {
+                
+        }
 }
