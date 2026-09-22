e7ea5e3b2b3834ac44c5da14e797b497a0d56ab9
simon0302010 <simon0302010@gmail.com>
2026-09-22 09:31:13 +0200

    feat: add Linux GUI support

--- src/gui/gui_linux.c
+++ src/gui/gui_linux.c
@@ -3,6 +3,9 @@ Source file for Linux GUI code.
 Needs to implement all functions defined in `gui.h`
 */
 
+#define NK_IMPLEMENTATION
+#include "nuklear/nuklear.h"
+
 int create_window(const char *title)
 {
         return 0;
--- src/main.c
+++ src/main.c
@@ -1,17 +1,3 @@
-#include <stdio.h>
-
 int main(void) {
-        int x;
-        int y;
-
-        x = 10;
-
-        printf("test");
-
-        y = 10;
-
-        if (x > 5)
-                printf("big\n");
-
         return 0;
 }
