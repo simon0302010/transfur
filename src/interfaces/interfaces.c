93ee7e4c59ad591919db3256ef0eacb3053a1f85
simon0302010 <simon@hackclub.app>
2026-09-24 19:31:01 +0200

    feat: implement connection management functions and update interface enum

--- src/interfaces/interfaces.c
+++ src/interfaces/interfaces.c
@@ -2,6 +2,11 @@
 
 #include "interfaces.h"
 
+#include "file/file.h"
+#include "lan/lan.h"
+#include "serial/serial.h"
+
+/* Connection slots */
 static unsigned char connections[MAX_CONN][512];
 static enum interface connection_types[MAX_CONN];
 
@@ -23,4 +28,86 @@ void *get_new_conn(enum interface if_type, int *conn) {
         }
 
         return NULL;
+}
+
+/*
+Retrives the context of a connection.
+Returns NULL if the requested connection doesn't exist.
+*/
+void *get_conn(const int *conn) {
+        if (*conn < 0 || *conn >= MAX_CONN ||
+            connection_types[*conn] == if_empty)
+                return NULL;
+
+        return connections[*conn];
+}
+
+/*
+Return values:
+
+0: Success
+1: Invalid interface
+2: Connection slots full
+*/
+int init_conn(int *conn, enum interface interface, void *options) {
+        void *context;
+
+        if (interface == if_empty) {
+                return 1;
+        }
+
+        context = get_new_conn(interface, conn);
+        if (context == NULL)
+                return 2;
+
+        switch (interface) {
+        case if_empty:
+                break; /* This shouldn't happen */
+        case if_file:
+                return init_conn_file(context, (const char *)options);
+        case if_serial:
+                return init_conn_serial(context, (const char *)options);
+        case if_lan:
+                return init_conn_lan(context, (const char *)options);
+        }
+
+        return 1;
+}
+
+int send_chunk(const int *conn, const struct chunk *chunk) {
+        void *context = get_conn(conn);
+        if (context == NULL)
+                return 1;
+
+        switch (connection_types[*conn]) {
+        case if_empty:
+                break;
+        case if_file:
+                return send_chunk_file(context, chunk);
+        case if_serial:
+                return send_chunk_serial(context, chunk);
+        case if_lan:
+                return send_chunk_lan(context, chunk);
+        }
+
+        return 1;
+}
+
+int recv_chunk(const int *conn, struct chunk *chunk) {
+        void *context = get_conn(conn);
+        if (context == NULL)
+                return 1;
+
+        switch (connection_types[*conn]) {
+        case if_empty:
+                break;
+        case if_file:
+                return recv_chunk_file(context, chunk);
+        case if_serial:
+                return recv_chunk_serial(context, chunk);
+        case if_lan:
+                return recv_chunk_lan(context, chunk);
+        }
+
+        return 1;
 }
