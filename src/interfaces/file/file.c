5f3fcc9baaf0eabe67708c59d4ba8cd74b54cad9
Jeremiah Saunders <jeremiahsaunders5@gmail.com>
2026-09-25 16:00:00 -0500

    feat: implement (draft) file interface

--- src/interfaces/file/file.c
+++ src/interfaces/file/file.c
@@ -9,3 +9,32 @@ If this cannnot be guaranteed, it must be clearly stated in the README and not c
 The receive function of the `file` interface reads from a file while the send function writes to a file.
 Only the receiving file interface is meant to process chunk types 1 and 2.
 */
+
+
+
+#define FILE_APPEND false
+
+int init_conn_file(void *conn, const char *file_path) {
+        strncpy(conn, file_path, 512 - 1);
+
+        return 0;
+}
+
+int send_chunk_file(void *conn, const struct chunk *chunk) {
+        FILE *file = fopen(conn, "a");
+
+        fwrite(&chunk, sizeof(chunk), 1, file);
+
+        return 0;
+}
+
+int recv_chunk_file(void *conn, struct chunk *chunk) {
+        FILE *file = fopen(conn, "r");
+
+        // TODO: replace 0 with actual size
+        // perhaps we add a header to the file that lists chunk size
+        // also, this doesn't account for existing chunks within the file
+        // perhaps that can be saved in conn
+        fread(*chunk, 0, 1, file);
+}
+
